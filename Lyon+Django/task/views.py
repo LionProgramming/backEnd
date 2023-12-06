28fb37273fb9db4745654e06333516642939b7c3
Andoret <dagar9908@gmail.com>
2023-12-06 10:19:47 -0500

    add: route observaciones id

--- Lyon+Django/task/views.py
+++ Lyon+Django/task/views.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from rest_framework.response import Response
 from .serializer import UserSerializer,HorariosSerializer,GradosSerializer,ObservacionesSerializer,RolesSerializer
 from .models import Usuarios,Horarios,Grados,Observaciones
 from django.db.models import Count
+from django.http import JsonResponse
 
 class UsuariosView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
@@ -18,13 +19,20 @@ class GradosView(viewsets.ModelViewSet):
     serializer_class=GradosSerializer
     queryset=Grados.objects.all()
 
-    
 class ObservacionesView(viewsets.ModelViewSet):
-    serializer_class=ObservacionesSerializer
-    queryset=Observaciones.objects.all()
+    serializer_class = ObservacionesSerializer
+    queryset = Observaciones.objects.all()
+    filter_backends = [filters.SearchFilter]
 
-from django.db.models import Count
-from django.http import JsonResponse
+    def get_queryset(self):
+        # Obtener el valor del parámetro 'usuario_documento' de la URL
+        usuario_documento = self.request.query_params.get('usuario_documento', None)
+
+        # Filtrar observaciones por el usuario_documento proporcionado
+        if usuario_documento is not None:
+            return Observaciones.objects.filter(usuario_documento__documento=usuario_documento)
+        else:
+            return Observaciones.objects.all()
 
 
 def contar_usuarios_por_rol(request):
